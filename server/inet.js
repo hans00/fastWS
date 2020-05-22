0a61e038a7f715a3512291880ab8a3b6828a1211
Hans <jy29825377@gmail.com>
2020-05-22 15:34:47 +0800

    Multiple change about IP formating
    fix: IP format bug hen bind on IPv4
    change: Shortening IPv6 address

--- server/inet.js
+++ server/inet.js
@@ -1,8 +1,10 @@
 const V4Prefix = Buffer.from([0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xff, 0xff])
 
 exports.ntop = (buf) => {
-  if (!Buffer.compare(buf.slice(0, 12), V4Prefix)) {
-    return buf.slice(12).join('.')
+  if (buf.length === 4) {
+    return buf.join('.') // to v4 string
+  } else if (!Buffer.compare(buf.slice(0, 12), V4Prefix)) {
+    return buf.slice(12).join('.') // to v4 string
   } else {
     return buf.reduce((str, num, i) => {
       str += num.toString(16).padStart(2, '0')
@@ -10,6 +12,8 @@ exports.ntop = (buf) => {
         str += ':'
       }
       return str
-    }, '')
+    }, '') // to full v6 string
+    .replace(/(0000|:0000)+/,':') // collapse multiple zeroes
+    .replace(/0+([\dabcdef]+)/g,'$1') // drop leading zeroes
   }
 }
