2a2064a366cfeba0d32a8ef8ab8d95636985e736
Hans <hans00@users.noreply.github.com>
2022-06-29 02:27:28 +0800

    Fix build script

--- build-arm.sh
+++ build-arm.sh
@@ -5,4 +5,4 @@ docker run \
   --rm \
   -v $PWD:/app \
   ghcr.io/prebuild/linux-arm64:2 \
-  bash -c 'alias yarn=npm && npm run pre-build-arm'
+  bash -c 'npm -w /app/packages/server run pre-build-arm'
