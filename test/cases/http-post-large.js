3bef1323d4f10989322efc2c1ced12aaafc0f0b6
Hans <hans00@users.noreply.github.com>
2022-07-23 01:46:55 +0800

    test(Server): fix test

--- test/cases/http-post-large.js
+++ test/cases/http-post-large.js
@@ -5,7 +5,7 @@ module.exports = async function ({ HTTP_PORT }) {
   const res = await axios({
     method: 'post',
     url: `http://localhost:${HTTP_PORT}/post`,
-    data: 'A'.repeat(2048000),
+    data: 'A'.repeat(1024 * 1024 + 1),
   }).catch(e => e.response)
   if (res.status !== 413) {
     throw new Error(`Response ${res.status}`)
